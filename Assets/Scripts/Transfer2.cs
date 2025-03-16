ec8f3caa0b7a51bc003ffecd7d0ae7d6a78b74be
HaoRUUU <tzhen030@uottawa.ca>
2025-03-16 17:46:50 -0400

    Unity-Setup
    stuff before share

---
+++ Assets/Scripts/Transfer2.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transfer2 : MonoBehaviour
+{
+    public float countTime = 0f; // 计时器
+
+    void Update()
+    {
+        CountTime(); // 在 Update 中调用计时方法
+    }
+
+    void CountTime()
+    {
+        countTime += Time.deltaTime; // 每帧增加时间
+        if (countTime >= 5f) // 如果计时器达到 5 秒
+        {
+            SceneManager.LoadScene(4); // 跳转到场景编号为 2 的场景
+        }
+    }
+}
